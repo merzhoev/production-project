31e49dd52b035293eca1a335bc03b34abe81dd6d
merzhoev <arbimerzhoev@gmail.com>
2023-03-01 18:36:54 +0300

    add: cart modal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { Header } from "components/header";
 import { Promo } from "components/promo";
 import { Products } from "components/products";
-import { Cart } from "components/cart";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Promo />
-      <Cart />
       <Products />
     </div>
   );
