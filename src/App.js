0085e2f82e89b1bf498d3a5e6244cafb86124955
merzhoev <arbimerzhoev@gmail.com>
2023-03-01 17:39:37 +0300

    add: all the world

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { Header } from "components/header";
+import { Promo } from "components/promo";
+import { Products } from "components/products";
+import { Cart } from "components/cart";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Promo />
+      <Cart />
+      <Products />
     </div>
   );
 }
---
+++ src/components/product-card/product-card.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import cartIcon from 'assets/images/cart.svg';
+import './product-card.scss';
+
+export function ProductCard({ id, imageUrl, title, price }) {
+  return (
+    <div key={id} className="products-card">
+      <img className="products-card__image" src={imageUrl} alt="card" />
+      <div className="products-card__title-container">
+        <h3 className="products-card__title">{title}</h3>
+        <span className="products-card__price">{price} Х</span>
+      </div>
+      <div className="products-card__details">
+        <button className="products-card__detail">
+          <img src={cartIcon} alt="cart" />
+        </button>
+      </div>
+    </div>
+  );
+}
