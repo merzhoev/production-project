64c36f9435600f98901f7813980f25d8966ca9ed
Adam Kuzgiev <kuzgiev99@gmail.com>
2023-03-02 11:18:20 +0300

    add: path

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
-import { CartModal } from 'components/cart-modal';
-import logoIcon from 'assets/images/logo.svg';
-import cartIcon from 'assets/images/cart.svg';
-import userIcon from 'assets/images/user.svg';
-import './header.scss';
+import React, { useState } from "react";
+import { CartModal } from "components/cart-modal";
+import logoIcon from "assets/images/logo.svg";
+import cartIcon from "assets/images/cart.svg";
+import userIcon from "assets/images/user.svg";
+import "./header.scss";
+import { Link } from "react-router-dom";
 
 export function Header() {
   const [isModalShown, setIsModalShown] = useState(false);
@@ -11,20 +12,28 @@ export function Header() {
   return (
     <header className="header">
       <div className="header__inner container">
-        <img src={logoIcon} alt="logo" />
+        <Link to="/" >
+          <img src={logoIcon} alt="logo" />
+        </Link>
         <div className="header__details">
           <span className="header__detail">235 Х</span>
           <button
             onClick={() => setIsModalShown(true)}
-            className="header__detail header__detail--button">
+            className="header__detail header__detail--button"
+          >
             <img src={cartIcon} alt="cart" />
           </button>
-          <button className="header__detail header__detail--button">
-            <img src={userIcon} alt="user" />
-          </button>
+          <Link to="/profile">
+            <button className="header__detail header__detail--button">
+              <img src={userIcon} alt="user" />
+            </button>
+          </Link>
         </div>
       </div>
-      <CartModal onClose={() => setIsModalShown(false)} isShown={isModalShown} />
+      <CartModal
+        onClose={() => setIsModalShown(false)}
+        isShown={isModalShown}
+      />
     </header>
   );
 }
