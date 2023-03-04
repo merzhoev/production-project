7fac9d0e8c01459105008ec83400d596d6c3a6e1
Adam Kuzgiev <kuzgiev99@gmail.com>
2023-03-04 15:32:51 +0300

    add: out button, browser icon

--- src/pages/profile-page/profile-page.jsx
+++ src/pages/profile-page/profile-page.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import './profile-page.scss';
-import userLogo from 'assets/images/user.svg';
-import { useSelector } from 'react-redux';
+import React from "react";
+import "./profile-page.scss";
+import userLogo from "assets/images/user.svg";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export function ProfilePage() {
   const user = useSelector((state) => state.user.data);
+  const navigate = useNavigate();
+
+  const handleOutClick = () => {
+    localStorage.removeItem("token");
+    navigate("/login") 
+  };
 
   return (
     <div className="container profile">
@@ -15,7 +22,7 @@ export function ProfilePage() {
           </div>
           <h2 className="profile__name">{`${user.last_name} ${user.first_name} ${user.patronymic}`}</h2>
           <p className="profile__team">
-            {' '}
+            {" "}
             Команда
             <br /> {user.team_name}
           </p>
@@ -23,6 +30,9 @@ export function ProfilePage() {
             <p className="profile__check">На счету:</p>
             <p className="profile__point"> {user.money} X</p>
           </div>
+          <button onClick={() => handleOutClick()} className="profile__out">
+            Выход
+          </button>
         </div>
       )}
     </div>
