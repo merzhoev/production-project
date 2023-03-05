0e007c1b3be2e26a68560931a0165642d1c9c9d7
merzhoev <arbimerzhoev@gmail.com>
2023-03-05 14:28:01 +0300

    add: aliases

--- webpack.config.ts
+++ webpack.config.ts
@@ -12,6 +12,7 @@ export default (env: BuildEnv): Configuration => {
       entry: path.resolve(__dirname, 'src', 'index.tsx'),
       build: path.resolve(__dirname, 'build'),
       html: path.resolve(__dirname, 'public', 'index.html'),
+      src: path.resolve(__dirname, 'src'),
     },
     isDev: mode === 'development',
     port: env.port || 3000,
